fce7ed4b9e9ae7bfdceeee4e384ad8761498cd91
Terence Lee <hone02@gmail.com>
2010-06-16 22:00:16 -0500

    test app

---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup
+
+require 'rack-cache-app'
+run RackCacheApp
---
+++ rack-cache-app.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+require 'memcached'
+require 'rack/cache'
+
+class RackCacheApp < Sinatra::Base
+  $cache = Memcached.new
+
+  use Rack::Cache,
+    :verbose => true
+
+  get "/" do
+    cache_control :public, :max_age => 2 * 60
+
+    "Cached at #{Time.now.to_s}"
+  end
+end
